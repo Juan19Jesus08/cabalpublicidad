d193ba5320218415b85ea5bcd99d83c7f326d004
Juan19Jesus08 <juanjesuspadrondiaz@gmail.com>
2019-08-16 14:53:51 -0500

    quinto commit viernes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -198,7 +198,7 @@ $array=CategoriaController::mostrar_categorias();
 foreach( $array as $item)
 {
 
-echo '	<li id="menu-item-871" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-871"><a title="All Courses" href="/cursos/">Curso con categoria:'. $item->cate.'</a></li>';
+echo '	<li id="menu-item-871" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-871"><a title="All Courses" href="/cursos/">'. $item->cate.'</a></li>';
 }
 
   ?>
