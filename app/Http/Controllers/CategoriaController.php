cb8c90851b8f468d847fb6fa0eedae96723ae58b
Juan19Jesus08 <juanjesuspadrondiaz@gmail.com>
2019-08-16 01:26:04 -0500

    septimo commit jueves

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+
 class CategoriaController extends Controller
 {
     
 	public function index()
 	{
-		$category=DB::select('SELECT categoria.descripcion FROM categoria');
-		return view ('welcome',compact('category'));
-    }
+		
+		$category=DB::select('SELECT categoria.descripcion as category FROM categoria');
+      
+        print_r ($category);
+		exit();
+		return view('welcome',compact('category'));
+		//return ('/Rol/Rol')->withData($data);
+		//return view('rol',compact('rol'));
+
+	}
     
 }
--- resources/views/principal/rol.blade.php
+++ resources/views/principal/rol.blade.php
@@ -6,34 +6,8 @@
 	<title></title>
 </head>
 <body>
-	<?php print_r($videos_populares);
-	echo"</br>";
-	echo"</br>";
-	echo"</br>";
-	print_r($videos_recientes);
+	<?php
+	print_r($category);
 	?>
-		<table border="1">
-		<thead>
-		<tr>
-			<th>nombre</th>
-			<th>descripcion</th>
-            <th>calificacion</th>
-            <th>vendidos</th>
-            <th>precio</th>
-		</tr>
-		</thead>
-		<tbody>
-			@foreach($videos_populares as $items)
-			<tr>
-				<td>{{$items->nombre}}</td>
-				<td>{{$items->descripcion}}</td>
-                <td>{{$items->calificacion_curso}}</td>
-                <td>{{$items->vendidos}}</td>
-                <td>{{$items->precio}}</td>
-			</tr>
-			@endforeach
-		</tbody>
-	</table>
-</body>
-</html>
+		
 @stop
