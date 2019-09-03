4a4d4fb2ccbbd9fbf612ff4f7a74f3e29d418743
Juan19Jesus08 <juanjesuspadrondiaz@gmail.com>
2019-09-03 02:00:23 -0500

    119 password

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Mail;
 use View;
-
+use DB;
 use Illuminate\Http\Request;
 use Swift_Mailer;
 use Swift_MailTransport;
@@ -12,7 +12,7 @@ use Swift_Message;
 
 class EmailController extends Controller
 {
-   public function email(){
+   public function email($email, $nombre, $password){
        // Configuration
        $smtpAddress = 'smtp.live.com';
        $port = 587;
@@ -30,7 +30,8 @@ $mailer = new \Swift_Mailer($transport);
        // Prepare content
        $view = View::make('email_template', [
            'message' => '<h1>A Juan le gusta la espesa y pegajosa miel de la peach ;) po!</h1>',
-           'message2'=>'jose se come toda del dick head '
+           'message2'=>$password,
+
        ]);
 
 
@@ -38,7 +39,7 @@ $mailer = new \Swift_Mailer($transport);
       
        // Send email
 $message = new \Swift_Message('Cambio de Contraseña');
-$message->setFrom([$yourEmail => 'Cabal Publicidad'])->setTo(["14030619@itcelaya.edu.mx" => "juanitoki"])->setBody($html, 'text/html');
+$message->setFrom([$yourEmail => 'Cabal Publicidad'])->setTo([$email => $nombre])->setBody($html, 'text/html');
 
            
        if($mailer->send($message)){
@@ -51,7 +52,30 @@ $message->setFrom([$yourEmail => 'Cabal Publicidad'])->setTo(["14030619@itcelaya
    public function obtener_contraseña(Request $input)
 	{
         $email = $input['email_show'];
-        echo $email;
+      
 
+        $query = "select * from usuario where email='$email'";
+        $data=DB::select($query);
+        $nombre=$data[0]->nombre;
+        
+        $cantidad= sizeof($data);
+      
+        if($cantidad>0)
+        {
+            $length=10;
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $charactersLength = strlen($characters);
+            $password = '';
+            for ($i = 0; $i < $length; $i++) {
+                $password .= $characters[rand(0, $charactersLength - 1)];
+                }
+            
+                $encryptedPassword = bcrypt($password);
+                $query2=DB::update("update  usuario set password='$encryptedPassword' where email=?",[$email]);
+            $this->email($email,$nombre,$password);
+        }
+        else{
+            return redirect('/mi_contraseña');
+        }
     }
 }
