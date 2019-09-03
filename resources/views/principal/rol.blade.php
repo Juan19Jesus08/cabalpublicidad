efbddc652fc81b1883e39bec5a984358ecaf14c3
Juan19Jesus08 <juanjesuspadrondiaz@gmail.com>
2019-09-03 17:44:51 -0500

    122 password

--- resources/views/principal/rol.blade.php
+++ resources/views/principal/rol.blade.php
@@ -1,10 +1,11 @@
 <?php
-
-print_r($cursos);
-
+   $dur = file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=$vId&key=AIzaSyBU1lZtClzanCeW37ILQ2UB70X-VatLSps");
+    $duration = json_decode($dur, true);
+     foreach ($duration['items'] as $vidTime) 
+     { 
+         $vTime= $vidTime['contentDetails']['duration'];
+    
+     }
 
 
 ?>
-<iframe width="560" height="315" src="https://www.youtube.com/embed/eUA8H9tkzlk?start=0&end=60&modestbranding=1&rel=0&showinfo=1&controls=0" frameborder="0"  allowfullscreen></iframe>
-
-    <iframe src="https://www.youtube.com/embed/HIsKF943vTk?disablekb=1&start=0&end=10" frameborder="0" allowfullscreen width="570" height="315" ></iframe>
