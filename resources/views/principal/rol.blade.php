b04c15ef783765f2eea16495ba8645d9179c9630
Juan19Jesus08 <juanjesuspadrondiaz@gmail.com>
2019-09-03 19:20:46 -0500

    122 password

--- resources/views/principal/rol.blade.php
+++ resources/views/principal/rol.blade.php
@@ -1,6 +1,6 @@
 <?php
     $apikey='AIzaSyBU1lZtClzanCeW37ILQ2UB70X-VatLSps';
-    $videoID='wCSOKiIasN0';
+    $videoID='63QywBbmENw';
    $dur = file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=contentDetails&id=$videoID&key=$apikey");
    $duration = json_decode($dur, true);
      foreach ($duration['items'] as $vidTime) 
@@ -8,7 +8,15 @@
          $vTime= $vidTime['contentDetails']['duration'];
     
      }
-     echo $vTime;
-     print_r( $duration);
+     //echo $vTime;
+     $resultado="";
+     $resultado =  str_replace("H", ":", $vTime);
+     $resultado =  str_replace("M", ":", $resultado);
+     $resultado=substr($resultado, 0, -1);
+     $resultado=substr($resultado,  2 );
+
+
+    echo "La cadena resultante es: " . $resultado;
+     //print_r( $duration);
 
 ?>
